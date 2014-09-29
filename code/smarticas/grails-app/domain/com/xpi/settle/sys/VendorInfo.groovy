4e88c773917b7fddf23c99874f13a84d74bb9953
Shunchao Wang <wang.2799@osu.edu>
2014-09-29 16:05:21 -0400

    Added vendor info domain.

---
+++ code/smarticas/grails-app/domain/com/xpi/settle/sys/VendorInfo.groovy
@@ -0,0 +1,52 @@
+package com.xpi.settle.sys
+
+class VendorInfo {
+
+    String vendorNo
+    Long orgId
+    String areaCode
+    String ownerName
+    Integer status
+    String connTel
+    String addr
+    String credCard
+    String businLicen
+    Date businOpenTime
+    Integer consumeLevel
+    String vendorName
+    Integer credLevel
+    String mobileNo
+    Integer chkStatus
+    Integer businType
+    String businCont
+    Integer signType
+    Integer dataOri
+    String vendorCode
+    String bankCardNo
+    BigDecimal longitude
+    BigDecimal latitude
+    Double feeRate
+    String accountNo
+    String accountMan
+    String businAddr
+    Long doRange
+    String lawMan
+    String memo
+    String connMan
+    Date openDate
+    Long createMan
+    Integer depositFlag
+    Integer hangFlag
+    Date hangDate
+    Date hangoffDate
+    Integer setupLocType
+    String vendSeq
+    String vendDeloper
+    String bindTel
+    Integer isxzc = 0
+    Integer diffFeeRate = 0
+    Integer isOnceBind = 0
+
+    static constraints = {
+    }
+}
